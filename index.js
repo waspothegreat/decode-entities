0ea766303a5f715d922d2329814d106f28898b67
Nicolas <nicolas.husereau@theojcs.ca>
2018-12-20 20:59:46 -0500

    Add all entities

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+const translationsObject = require('./entities.js');
+
 /**
  * Decodes (for the most part) HTML entities
  * @param {string} encodedString The string to decode
- * @returns {string} The clean string with some entities decoded, any others that couldn't be replaced will be removed.
+ * @returns {string} The clean string with some entities decoded
  */
 module.exports = (encodedString) => {
 	const translateRegex = /&(nbsp|amp|quot|lt|gt);/g;
-	const translateObject = {
-		nbsp: ' ',
-		amp: '&',
-		quot: '"',
-		lt: '<',
-		gt: '>',
-	};
+	const translateObject = translationsObject;
 
 	return encodedString.replace(translateRegex, (match, entity) => translateObject[entity]).replace(/&#(\d+);/gi, (match, numStr) => {
 		const num = parseInt(numStr, 10);
